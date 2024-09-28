7ee8862495d7bdf533f71a59a6b12a5d6fec898f
shubham <49603826+shubhdevelop@users.noreply.github.com>
2024-09-28 06:51:26 +0530

    added tanstack table

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Button } from '@/components/ui/button';
 import { signOut, useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import React from 'react'
@@ -8,14 +9,11 @@ function Profile() {
     const { status } = useSession();
     if (status !== "authenticated") redirect("/sign-in")
     return (
-        <div className='w-full h-full m-3 bg-red-600' >
-            <button className='bg-red-500 border-[.3px]px - 3 py - 2 text - white'
-                onClick={() => signOut()
-                }>
-                Sign out
-            </button >
-        </div >
-
+        <Button className='bg-red-500 border-[.3px]px - 3 py - 2 text - white'
+            onClick={() => signOut()
+            }>
+            Sign out
+        </Button >
     )
 }
 
